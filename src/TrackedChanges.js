c1e69f0a0dc40a111ec7f1e359cf623e1d7a3a0a
Lars Georgi - deltra-Team <lg@deltra.de>
2017-07-13 16:05:52 +0200

    Step between

--- src/TrackedChanges.js
+++ src/TrackedChanges.js
@@ -1,28 +1,56 @@
 import React from 'react';
 import TrackedChangesAddBar from './TrackedChangesAddBar';
 import TrackedChangesList from './TrackedChangesList';
+const chokidar = window.require('chokidar');
 
 class TrackedChanges extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            list: []
+            trackedLocations: localStorage.getItem('trackedLocations') || [],
+            observedList: [{
+                folder: '',
+                items: []
+            }]
         }
+        this.addEvent = this.addEvent.bind(this);
+    }
+
+    addItem(ident, path) {
+        const dir = path.substring(0,path.lastIndexOf("\\")+1);
+        var folder = this.state.observedList.find((item) => item.folder == dir);        
+        var folderIndex = -1;
+        if (folder != undefined)
+            folderIndex = this.state.observedList.indexOf(folder);
+
+        const newList = this.state.observedList;
+        if (folderIndex >= 0)
+            newList[folderIndex].items.unshift(ident + ' ' + path);
+        else
+            newList.unshift({folder: dir, items: []});
+
+        setState('observedList', newList)
+    }
+
+    addEvent(path) {
+        addItem('added', path);
     }
 
     addItem (item) {
-        const newList = this.state.list.concat([item]);
-
+        const newList = this.state.trackedLocations.concat([item]);
         localStorage.setItem('trackedLocations', JSON.stringify(newList));
 
-        this.setState({ list: newList })
+        this.setState({ trackedLocations: newList })
+
+        chokidar.watch(item, {ignoreInitial: true})
+            .on('add', this.addEvent);
     }
 
     render() {
         return(
             <div>
                 <TrackedChangesAddBar onAddItem={this.addItem.bind(this)} />
-                <TrackedChangesList data={this.state.list} />
+                <TrackedChangesList data={this.state.observedList} />
             </div>
         );
     }
--- src/TrackedChangesList.js
+++ src/TrackedChangesList.js
@@ -2,27 +2,10 @@ import React from 'react';
 import TrackedChangeRow from './TrackedChangeRow';
 
 class TrackedChangesList extends React.Component {
-    render() {
-        const { data } = this.props
-        // const rows = data.map(item => item)
-        var rows = [];
-
-        // var trackedLocations = JSON.parse(localStorage.getItem('trackedLocations')) || [];
-       
-        // trackedLocations.forEach((change) => {
-        //     rows.push(<TrackedChangeRow value={change} key={change.toString()} />)
-        // });
+    render() {        
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Value</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {rows}
-                </tbody>
-            </table>
+            
+                   
         );
     }
 }
