ce58f66ce970a94e0477329945e852dbaf5dbeb1
Lars Georgi <lgeorgi@gmx.de>
2017-07-15 20:48:28 +0200

    Fixed an issue with the visualisation

--- src/TrackedChanges.js
+++ src/TrackedChanges.js
@@ -9,38 +9,50 @@ class TrackedChanges extends React.Component {
         super(props)
 
         this.state = {
-            'observedList': JSON.parse(localStorage.getItem('observedItems')) || []            
+            'workList': []
         }
 
         this.addEvent = this.addEvent.bind(this);
         this.addTrackedLocation = this.addTrackedLocation.bind(this);
         this.addObservedItem = this.addObservedItem.bind(this);   
+        this.activateObservedItem = this.activateObservedItem.bind(this);
 
-        this.state.observedList.forEach((item) => {
-            this.activateObservedItem(item.folder);
-        });        
+        this.observedItems = JSON.parse(localStorage.getItem('observedItems')) || [];
     }    
 
+    componentDidMount() {
+        this.observedItems.forEach((item) => {
+            this.activateObservedItem(item);
+        });        
+    }
+
     activateObservedItem(location) {
-        chokidar.watch(location, {'ignoreInitial': false})
+        chokidar.watch(location, {'ignoreInitial': true, depth: 1})
             .on('add', this.addEvent);
     }
 
     addObservedItem(ident, location) {
-        const dir = location.substring(0, path.lastIndexOf(path.sep));
+        const dir = location.substring(0, location.lastIndexOf(path.sep));
         const filename = location.replace(/^.*[\\\/]/, '')
-        var folder = this.state.observedList.find((item) => item.folder == dir);
+        var folder = this.state.workList.find((item) => item.folder == dir);
         var folderIndex = -1;
         if (folder != undefined)
-            folderIndex = this.state.observedList.indexOf(folder);
+            folderIndex = this.state.workList.indexOf(folder);
 
-        var newList = this.state.observedList;
-        if (folderIndex >= 0)
-            newList[folderIndex].items.unshift(ident + ' ' + filename);
-        else{
-            newList.unshift({folder: dir, items: []});
+        var newList = this.state.workList;
+        var item = undefined;
+        if (folderIndex >= 0){
+            item = newList[folderIndex] ;
+            item.items.unshift(ident + ' ' + filename);
+            newList[folderIndex] = item;
         }
-        this.setState({'observedList': newList}); 
+        else{
+            item = {folder: dir, items: []};
+            item.items.push(ident + ' ' + filename);
+            newList.unshift(item);
+        }
+
+        this.setState({'workList': newList});
     }
 
     addEvent(location) {
@@ -48,10 +60,9 @@ class TrackedChanges extends React.Component {
     }
  
     addTrackedLocation(item) {
-        const newList = this.state.observedList || [];
-        newList.unshift({'folder': item});
-        localStorage.setItem('observedItems', JSON.stringify(newList));
-        this.setState({'observedList': newList}); 
+        this.observedItems.unshift(item);
+        localStorage.setItem('observedItems', JSON.stringify(this.observedItems));
+
         this.activateObservedItem(item);
     }
 
@@ -59,7 +70,7 @@ class TrackedChanges extends React.Component {
         return(
             <div>
                 <TrackedChangesAddBar onAddItem={this.addTrackedLocation} />
-                <TrackedChangesList data={this.state.observedList} />
+                <TrackedChangesList data={this.state.workList} />
             </div>
         );
     }
--- src/TrackedChangesList.js
+++ src/TrackedChangesList.js
@@ -2,18 +2,8 @@ import React from 'react';
 import TrackedChangesLocation from './TrackedChangesLocation';
 
 class TrackedChangesList extends React.Component {
-    constructor(props){
-        super(props);        
-        this.renderItems = props.data.map((item) => {
-            if (!item.items)
-                item.items = []
-            return (
-                item
-            )
-        })
-    }
     render() {  
-        const renderItems = this.renderItems.map((item) => {
+        const renderItems = this.props.data.map((item) => {
             return (     
                 <TrackedChangesLocation data={item} key={item.toString()}/>                
             )
